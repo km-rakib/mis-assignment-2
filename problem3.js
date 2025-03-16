779727274a9d4f3152e0694129452157c8382bba
km-rakib <kazimdrakibulhassan@gmail.com>
2025-03-16 13:29:17 +0600

    problem 3 completed

--- problem3.js
+++ problem3.js
@@ -1,3 +1,22 @@
 // 3.     
 // Ask for three number from the user. Display max/greatest
 // number in the console.
+
+var num1 = parseFloat(prompt("Enter the frist number: "));
+var num2 = parseFloat(prompt("Enter the second number: "));
+var num3 = parseFloat(prompt("Enter the third number: "));
+
+if(num1==num2 | num1 == num3 | num2 == num3){
+  console.log("Two numbers are equal don't enter equal numbers!!")
+}
+else{
+  if((num1>num2)&&(num1>num3)){
+    console.log("First number you have entered is the gretest number which is :", num1);
+  }
+  else if((num2>num1)&&(num2>num3)){
+    console.log("Second number you have entered is the gretest number which is :", num2);
+  }
+  else{
+    console.log("Second number you have entered is the gretest number which is :", num3);
+  }
+}
