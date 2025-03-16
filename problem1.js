83f75dec02b7e7f890aef15489c366c5ce066fa0
km-rakib <kazimdrakibulhassan@gmail.com>
2025-03-16 13:07:44 +0600

    problem 1 solved

--- problem1.js
+++ problem1.js
@@ -1,3 +1,11 @@
 // 1.     
 // Ask for two numbers from the user. Display
 // greater number in the console.
+
+var num1 = parseFloat(prompt("Enter the first number: "));
+var num2 = parseFloat(prompt("Enter the second number: "));
+if(num1>num2){
+  console.log(num1, " is the greater number");
+}else{
+  console.log(num2, " is the greater number");
+}
