ca7d7ac1ecc17a6ac051fc193945d6a61c998e30
km-rakib <kazimdrakibulhassan@gmail.com>
2025-03-16 13:14:42 +0600

    problem 2 solved

--- problem2.js
+++ problem2.js
@@ -1,5 +1,13 @@
-
 // 2.     
 // Ask for age from the user, Display – whether he/she
 // is eligible to cast vote or not. (note – age has to be at least 18 to exercise voting
 // right)
+
+var age = parseFloat(prompt("Enter your age: "));
+if(age>=18){
+  document.getElementById("vote").innerText = "You are eligable for vote";
+  console.log("You are eligable for vote");
+}else{
+  document.getElementById("vote").innerText = "You are not eligable for vote";
+  console.log("You are not eligable for vote");
+}
